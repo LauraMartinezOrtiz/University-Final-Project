531dde77bc5381a19161bd59d51af5b851d7fa7e
Laura Martinez Ortiz <l.martinezortiz@globant.com>
2024-08-12 19:16:27 -0500

    Feat(student): class added

---
+++ src/data/Student.java
@@ -0,0 +1,29 @@
+package src.data;
+
+public class Student extends Person {
+
+    private long id;
+    private int age;
+
+    public Student(String name, int age, long id) {
+        super(name);
+        this.age = age;
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
